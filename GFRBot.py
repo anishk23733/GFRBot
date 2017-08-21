fcf29dee9f8b49a87685ec042eb52577f0090234
Devin Mui <devinmui@yahoo.com>
2017-08-20 20:27:39 -0700

    literally kill myself

--- GFRBot.py
+++ GFRBot.py
@@ -114,184 +114,9 @@ async def random_cat(ctx, num: int = 1):
 				res = await r.json()
 				await client.say(res['file'])
 
-
-# Define Command
-@client.command(pass_context=True)
-async def manual(ctx, page: int = 0):
-	text = manual(page)
-	await client.say(text)
-
-def manual(page: int = 0):
-	# web_file = urllib.request.urlopen("https://content.vexrobotics.com/docs/vrc-inthezone/VRC-InTheZone-GameManual-20170817.pdf")
-	# local_file = open('manual.pdf', 'wb')
-	# local_file.write(web_file.read())
-	file = open('manual.pdf','rb')
-	pdfReader = PyPDF2.PdfFileReader(file)
-	pageObj = pdfReader.getPage(page)
-	text = pageObj.extractText()
-	return text.replace("\n","") \
-		.replace("VEX Robotics Competition In the Zone Œ Game Manual    vexrobotics.com Copyright 2016, VEX Robotics Inc. 2017-08-17","") \
-		.replace("vexrobotics.com Copyright 2017, VEX Robotics Inc. 2017-08-17","")
-
-
-@client.command(pass_context=True)
-async def find(ctx, word):
-	if(word is None):
-		await client.say("You didn't pass in a word")
-	else:
-		await client.say("Found instance of "+ word + " in sections: " + ', '.join(findInSection(word)))
-
-def find(word):
-	found = [];
-	NUM_OF_PAGES = 35
-	for i in range(NUM_OF_PAGES):
-		if word in manual(i):
-			found.append(i)
-	return found
-
-def findInSection(word):
-	word = word.lower()
-	print(word)
-	RULE_PAGES = {
-		"start": 13,
-		"end": 20
-	}
-
-	sections = ["<S1>","<S2>"]
-	# general
-	for i in range(1,18+1):
-		sections.append("<G"+str(i)+">")
-	# specific
-	for i in range(1,15+1):
-		sections.append("<SG"+str(i)+">")
-	# t
-	for i in range(1,5+1):
-		sections.append("<T0"+str(i)+">")
-	# robot
-	for i in range(1,20+1):
-		sections.append("<R"+str(i)+">")
-
-	texts = ""
-	# normal
-	for page in range(13-1,16+1):
-		text = manual(page)
-		text = text.replace("<SG3>","")\
-			.replace(text[text.find("A Cone Stacked on a Goal is worth two"):text.find("the most points receives a ten (10) point bonus.")+len("the most points receives a ten (10) point bonus.")],"")
-		texts += text
-	# sg
-	for page in range(17-1,19+1):
-		text = manual(page)
-		texts += text
-	# t
-	for page in range(24,26):
-		text = manual(page)
-		bs = text[text.find("Small Tournaments (Level 1 Tournaments)"):text.find("12 Alliances of 2 teams")+len("12 Alliances of 2 teams")]
-		otherBs = text[text.find("This section provides rules and requirements"):text.find("Please ensure that you are familiar with each of these robot rules before proceeding with robot design.")+len("Please ensure that you are familiar with each of these robot rules before proceeding with robot design.")]
-		text = text.replace(bs,"")\
-			.replace(text[text.find("The Elimination Matches"):text.find("two wins, and advances.")+len("two wins, and advances.")],"")\
-			.replace(otherBs,"")
-		texts += text
-	# r
-	for page in range(28-1, 35):
-		text = manual(page)
-		texts += text
-
-	texts = texts.lower()
-	found = []
-
-	for i in range(len(sections)-1):
-		s = None
-		current = sections[i].lower()
-		if(i < len(sections)-1-1):
-			nxt = sections[i+1].lower()
-			s = texts[texts.find(current):texts.find(nxt)]
-		else:
-			s = texts[texts.find(current):len(texts)]
-
-		if(sections[i] == "<SG2>"):
-			print(s)
-			nxt = sections[i+1]
-			print(str(texts.find(current))+":"+str(texts.find(nxt)))
-			print(nxt)
-
-		texts = texts.replace(s,"")
-
-		if word in s:
-			found.append(sections[i])
-
-	return found
-
-
-@client.command(pass_context=True)
-async def rule(ctx, rule):
-	if(rule is None):
-		await client.say("You didn't pass in a rule")
-	else:
-		if not rule.startswith("<"):
-			rule = "<" + rule + ">"
-		rule = rule.upper()
-		embed = Embed(title=rule,type="rich",description=getRule(rule),color=discord.Colour.teal())
-
-		await client.send_message(ctx.message.channel, embed=embed)
-
-def getRule(section):
-	sections = []
-
-	section = section.upper()
-	texts = ""
-	if section.startswith("G") or section.startswith("<G"):
-		for i in range(1,19+1):
-			sections.append("<G"+str(i)+">")
-		for page in range(12,16+1):
-			text = manual(page)
-			text = text.replace(text[text.find("A Cone Stacked on a Goal is worth two"):text.find("inadvertently cross the field border during normal game play.")+len("inadvertently cross the field border during normal game play.")],"")\
-				.replace(text[text.find("<SG1> At the"):text.find("Figure 15 (right): A Legal Preload")+len("Figure 15 (right): A Legal Preload")],"")
-			texts += text
-	elif section.startswith("S") or section.startswith("<S"):
-		sections = ["<S1>","<S2>"]
-		for i in range(1,16+1):
-			sections.append("<SG"+str(i)+">")
-		for page in range(17-1,19+1):
-			text = manual(page)
-			texts += text
-	elif section.startswith("T") or section.startswith("<T"):
-		for i in range(1,6+1):
-			sections.append("<T0"+str(i)+">")
-		for page in range(24,26):
-			text = manual(page)
-			bs = text[text.find("Small Tournaments (Level 1 Tournaments)"):text.find("12 Alliances of 2 teams")+len("12 Alliances of 2 teams")]
-			otherBs = text[text.find("This section provides rules and requirements"):text.find("Please ensure that you are familiar with each of these robot rules before proceeding with robot design.")+len("Please ensure that you are familiar with each of these robot rules before proceeding with robot design.")]
-			text = text.replace(bs,"")\
-				.replace(text[text.find("The Elimination Matches"):text.find("two wins, and advances.")+len("two wins, and advances.")],"")\
-				.replace(otherBs,"")
-			texts += text
-	elif section.startswith("R") or section.startswith("<R"):
-		for i in range(1,22+1):
-			sections.append("<R"+str(i)+">")
-		for page in range(28-1, 35):
-			text = manual(page)
-			texts += text
-
-	for i in range(len(sections)-1):
-		s = None
-		current = sections[i]
-		if(i < len(sections)-1-1):
-			nxt = sections[i+1]
-			s = texts[texts.find(current):texts.find(nxt)]
-		else:
-			s = texts[texts.find(current):len(texts)]
-
-		if(sections[i] == section):
-			return s
-
-		texts = texts.replace(s,"")
-
-	return "Not found"
-
 ########################
 # Mathemetical Commands#
 ########################
-
 
 # Math Check Command
 @client.command(pass_context=True)
@@ -369,6 +194,82 @@ async def img(ctx, loc="local.jpg", config=""):
 async def vex(ctx):
 	if ctx.invoked_subcommand is None:
 		await client.say('Invalid vex command passed...')
+
+@vex.command(pass_context=True)
+async def rules(ctx, rule=None):
+	jsonPath = "json"
+	rule = rule.replace("<","").replace(">","").lower()
+	rules = None
+	if rule.startswith('g'):
+		rules = open(jsonPath+'/g.json')
+	elif rule.startswith('r'):
+		rules = open(jsonPath+'/r.json')
+	elif rule.startswith('sg'):
+		rules = open(jsonPath+'/sg.json')
+	elif rule.startswith('s'):
+		rules = open(jsonPath+'/s.json')
+	elif rule.startswith('t'):	
+		rules = open(jsonPath+'/t.json')
+	if rules.closed:
+		embed = Embed(title="Invalid rule",type="rich",description="I don't think this is a rule",color=discord.Colour.red())
+		await client.send_message(ctx.message.channel, embed=embed)
+	else:
+		datas = json.load(rules)
+		definition = None
+		for data in datas:
+			if(rule == data["name"]):
+				definition = data["definition"]
+				break
+		if definition:
+			embed = Embed(title="<"+rule.upper()+">",type="rich",description=definition,color=discord.Colour.dark_teal())
+			await client.send_message(ctx.message.channel, embed=embed)
+		else:
+			embed = Embed(title="Could not find rule",type="rich",description="I don't think this is a rule",color=discord.Colour.red())
+			await client.send_message(ctx.message.channel, embed=embed)
+		rules.close()
+
+@vex.command(pass_context=True)
+async def manual(ctx, page: int = 0):
+	text = manual(page)
+	await client.say(text)
+
+def manual(page: int = 0):
+	file = open('manual.pdf','rb')
+	pdfReader = PyPDF2.PdfFileReader(file)
+	pageObj = pdfReader.getPage(page)
+	text = pageObj.extractText()
+	return text.replace("\n","") \
+		.replace("VEX Robotics Competition In the Zone Œ Game Manual    vexrobotics.com Copyright 2016, VEX Robotics Inc. 2017-08-17","") \
+		.replace("vexrobotics.com Copyright 2017, VEX Robotics Inc. 2017-08-17","")
+
+@vex.command(pass_context=True)
+async def define(ctx, word=None):
+	jsonPath = 'json'
+	if not word:
+		embed = Embed(title="Error",type="rich",description="No word selected",color=discord.Colour.red())
+		await client.send_message(ctx.message.channel, embed=embed)
+	else:
+		definit = open(jsonPath+'/definitions.json')
+		definitions = json.load(definit)
+		wordDefinition = None
+		for definition in definitions:
+			if word.lower() == definition["name"].lower():
+				wordDefinition = definition
+				break
+
+		if wordDefinition:
+			embed = Embed(title=wordDefinition["name"],type="rich",description=wordDefinition["definition"],color=discord.Colour.teal())
+			await client.send_message(ctx.message.channel, embed=embed)
+			definit.close()
+		else:
+			embed = Embed(title="Could not find word",type="rich",description="I don't think this is in the manual",color=discord.Colour.red())
+			await client.send_message(ctx.message.channel, embed=embed)
+			definit.close()
+
+
+@vex.command(pass_context=True)
+async def find(ctx, word=None):
+	pass
 
 @vex.command(pass_context=True)
 async def help(ctx):
